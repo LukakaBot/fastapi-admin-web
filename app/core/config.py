c137ca0c09e0332837dac52c52f42d4a8e64a2d9
鲨鱼辣椒 <monishine_code@163.com>
2025-03-27 11:20:47 +0800

    feat: 接口前缀添加默认值

--- app/core/config.py
+++ app/core/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
         extra="ignore",
     )
 
-    BACKEND_SERVICE_PREFIX: str = ""
+    BACKEND_SERVICE_PREFIX: str = "/api"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
