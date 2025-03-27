bc7c05e29b1ffca2a9a805c78f63a03e3bf96360
鲨鱼辣椒 <monishine_code@163.com>
2025-03-27 16:01:31 +0800

    feat: 移除获取用户信息接口

--- app/api/routes/login.py
+++ app/api/routes/login.py
@@ -1,47 +1,28 @@
-from fastapi import APIRouter, Header
+from fastapi import APIRouter
 from faker import Faker
-from typing import Annotated
 from app.model.common import ResponseData
 from app.model.login import AccountUserTokenParams
 
-router = APIRouter(tags=["login"])
+router = APIRouter(prefix="/login", tags=["login"])
 
 fake = Faker()
 
 
-@router.post("/user/account/token")
-def get_access_token(form_data: AccountUserTokenParams):
+@router.post("/account/access-token")
+def get_account_access_token(form_data: AccountUserTokenParams):
+    if form_data.username != "admin" or form_data.password != "123456":
+        return ResponseData(
+            code=401,
+            data=None,
+            message="用户名或密码错误",
+            type="error",
+        )
+
     return ResponseData(
         code=200,
         data={
             "userId": fake.random_int(min=100000, max=999999),
             "username": fake.name(),
             "token": fake.uuid4(),
-        },
-    )
-
-
-@router.get("/user/info")
-def get_user_info(Authorization: Annotated[str | None, Header()] = None):
-    if not Authorization:
-        return ResponseData(
-            code=401,
-            data=None,
-            message="Unauthorized",
-            type="error",
-        )
-    return ResponseData(
-        code=200,
-        data={
-            "userId": fake.random_int(min=100000, max=999999),
-            "userName": fake.name(),
-            "avatar": fake.image_url(),
-            "phone": fake.phone_number(),
-            "roles": ["admin"],
-            "roleName": "admin",
-            "menus": [],
-            "token": fake.uuid4(),
-            "organizeId": fake.random_int(min=100000, max=999999),
-            "organizeName": fake.company(),
         },
     )
